5ca490fb9874de44ad96570c1f78dbbd78972455
jerpen80 <dra_penny@yahoo.com>
2024-07-02 12:24:51 +0200

    added json load in python for variable

--- lambda_cost_alerter_setup/lambda_function.py
+++ lambda_cost_alerter_setup/lambda_function.py
@@ -15,7 +15,7 @@ sts_client = boto3.client("sts")
 # except FileNotFoundError:
 #     budget_thresholds = {}
 
-budget_thresholds = os.environ('BUDGET_THRESHOLDS')
+budget_thresholds = json.loads(os.environ('BUDGET_THRESHOLDS'))
 
 
 management_account_id = sts_client.get_caller_identity()["Account"]
