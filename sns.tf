5ebaf03e1fa38d30e39c5f0b5dd54ddfe448a330
andrew@technative.eu <andrew@technative.eu>
2024-12-03 11:29:04 +0100

    Updated module references to point to Wearetechnative, removed unused files

--- sns.tf
+++ sns.tf
@@ -1,6 +1,6 @@
 module "sns_budget" {
-  source = "git@github.com:TechNative-B-V/terraform-aws-module-sns.git?ref=fbee551a075c5dc895eb0ad36ed722d6292c0388"
-
+  source = "git@github.com:wearetechnative/terraform-aws-sns.git?ref=9ba0b25ee91220deafa243d14b0ba935fad6cb85"
+  
   name        = "sns_budget"
   kms_key_arn = null
   policy_allowed = {
